ff9a3dd91cca2ff38de82bb61db6b7356885c466
M Angeles Cordoba <mangeles.cg91@gmail.com>
2024-08-05 20:30:09 +0200

    Basic forms - Validación de campos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
    // Otra manera de crear el formulario 
     // public myForm: FormGroup = new FormGroup({
@@ -22,6 +22,35 @@ export class BasicPageComponent {
             price: [0, [Validators.required, Validators.min(0) ]],
             inStorage: [0, [Validators.required, Validators.min(0) ]],
         });
+    }
+
+    ngOnInit(): void {
+        this.myForm.reset({ price: 0, inStorage: 0 });
+    }
+
+    isValidField( field:string ): boolean | null {
+      return  this.myForm.controls[field].errors 
+              && this.myForm.controls[field].touched;
+    }
+
+    getFieldError( field:string ): string | null {
+
+      if ( !this.myForm.controls[field] ) return null; 
+
+      const errors = this.myForm.controls[field].errors || {}; 
+
+      for (const key of Object.keys(errors)) {
+        switch( key ) {
+          case 'required':
+            return 'Este campo es requerido';
+
+          case 'minlength':
+            return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;           
+        }
+      }
+
+      return null;
+      
     }
 
     onSave():void  {
